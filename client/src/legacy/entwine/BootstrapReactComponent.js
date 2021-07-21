eddfebf4ad058b405bf08b86e493cbaf88f11cdc
Maxime Rainville <maxime@silverstripe.com>
2021-07-21 15:56:05 +1200

    Wait for injector to be ready

--- client/src/legacy/entwine/BootstrapReactComponent.js
+++ client/src/legacy/entwine/BootstrapReactComponent.js
@@ -3,47 +3,49 @@
 import jQuery from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { loadComponent } from 'lib/Injector';
+import Injector, { loadComponent } from 'lib/Injector';
 
-jQuery.entwine('ss', ($) => {
-  $('.js-injector-boot .bootstrap-component').entwine({
+Injector.ready(() => {
+  jQuery.entwine('ss', ($) => {
+    $('.js-injector-boot .bootstrap-component').entwine({
 
-    Component: null,
+      Component: null,
 
-    onmatch() {
-      const cmsContent = this.closest('.cms-content').attr('id');
-      const context = (cmsContent)
-        ? { context: cmsContent }
-        : {};
+      onmatch() {
+        const cmsContent = this.closest('.cms-content').attr('id');
+        const context = (cmsContent)
+          ? { context: cmsContent }
+          : {};
 
-      const componentName = this.data('component');
-      const Component = loadComponent(componentName, context);
+        const componentName = this.data('component');
+        const Component = loadComponent(componentName, context);
 
-      this.setComponent(Component);
-      this._super();
-      this.refresh();
-    },
+        this.setComponent(Component);
+        this._super();
+        this.refresh();
+      },
 
-    refresh() {
-      const props = this.getProps();
-      const Component = this.getComponent();
-      ReactDOM.render(<Component {...props} />, this[0]);
-    },
+      refresh() {
+        const props = this.getProps();
+        const Component = this.getComponent();
+        ReactDOM.render(<Component {...props} />, this[0]);
+      },
 
-    /**
-     * Find the selected node and get attributes associated to attach the data to the form
-     *
-     * @returns {Object}
-     */
-    getProps() {
-      return $(this).data('props') || {};
-    },
+      /**
+       * Find the selected node and get attributes associated to attach the data to the form
+       *
+       * @returns {Object}
+       */
+      getProps() {
+        return $(this).data('props') || {};
+      },
 
-    /**
-     * Remove the component when unmatching
-     */
-    onunmatch() {
-      ReactDOM.unmountComponentAtNode(this[0]);
-    },
+      /**
+       * Remove the component when unmatching
+       */
+      onunmatch() {
+        ReactDOM.unmountComponentAtNode(this[0]);
+      },
+    });
   });
 });
